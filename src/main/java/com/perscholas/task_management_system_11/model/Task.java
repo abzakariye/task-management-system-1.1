2d39fa399498adad2c286534f0b10e2f501d3ce3
abdiza01 <abdilezakariye@gmail.com>
2024-09-26 23:37:40 -0400

    Implemented model layer

---
+++ src/main/java/com/perscholas/task_management_system_11/model/Task.java
@@ -0,0 +1,21 @@
+package com.perscholas.task_management_system_11.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private String status;
+}
