00cd610ff9448f6f84dc6955b9c98b709de8f2c7
abdiza01 <abdilezakariye@gmail.com>
2024-09-27 00:10:06 -0400

    Changed controller layer to work with Thymeleaf

--- src/main/java/com/perscholas/task_management_system_11/controller/TaskController.java
+++ src/main/java/com/perscholas/task_management_system_11/controller/TaskController.java
@@ -4,11 +4,15 @@ import com.perscholas.task_management_system_11.model.Task;
 import com.perscholas.task_management_system_11.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-@RestController
+// @RestController // This is for REST ENDPOINT CONTROLLER (POSTMAN)
+@Controller
 @RequestMapping("/tasks")
 public class TaskController {
 
@@ -16,35 +20,47 @@ public class TaskController {
     private TaskService taskService;
 
     @GetMapping
-    public List<Task> getAllTasks() {
-        return taskService.getAllTasks();
+    public String getAllTasks(Model model) {
+        List<Task> tasks = taskService.getAllTasks();
+        model.addAttribute("tasks", tasks);
+        return "tasks-list";
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
-        return taskService.getTaskById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.noContent().build());
+    @GetMapping("/new")
+    public String showCreateTaskForm(Model model) {
+        Task task = new Task();
+        model.addAttribute("task", task);
+        return "task-form";
     }
 
-    @PostMapping
-    public Task createTask(@RequestBody Task task) {
-        return taskService.createTask(task);
-    }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task task) {
-        try {
-            Task updatedTask = taskService.updateTask(id, task);
-            return ResponseEntity.ok(updatedTask);
-        }catch (RuntimeException e){
-            return ResponseEntity.noContent().build();
+
+    @GetMapping("/edit{id}")
+    public String showEditTaskForm(@PathVariable Long id, Model model) {
+        Optional<Task> task = taskService.getTaskById(id);
+        if (task.isPresent()) {
+            model.addAttribute("task", task.get());
+        }else {
+            return "redirect:/tasks";
         }
+        return "task-form";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
-        taskService.deleteTaskById(id);
-        return ResponseEntity.noContent().build();
+    @PostMapping("/save")
+    public String saveTask(@ModelAttribute("task") Task task) {
+        if (task.getId() != null) {
+            taskService.updateTask(task.getId(), task);
+        }else {
+            taskService.createTask(task);
+        }
+
+        return "redirect:/tasks";
     }
+
+   @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable Long id) {
+        taskService.deleteTaskById(id);
+        return "redirect:/tasks";
+   }
+
 }
