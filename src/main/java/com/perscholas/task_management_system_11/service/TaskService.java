f289bb5f6052e4f44fe5b160e315d361ccd78230
abdiza01 <abdilezakariye@gmail.com>
2024-09-26 23:40:44 -0400

    Implemented service layer

---
+++ src/main/java/com/perscholas/task_management_system_11/service/TaskService.java
@@ -0,0 +1,43 @@
+package com.perscholas.task_management_system_11.service;
+
+import com.perscholas.task_management_system_11.model.Task;
+import com.perscholas.task_management_system_11.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTaskById(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public Task updateTask(Long id, Task updatedTask) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(updatedTask.getTitle());
+                    task.setDescription(updatedTask.getDescription());
+                    task.setStatus(updatedTask.getStatus());
+                    return taskRepository.save(task);
+                })
+                .orElseThrow(()-> new RuntimeException("Task not found"));
+    }
+}
